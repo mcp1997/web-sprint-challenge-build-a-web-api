e07a0039c9c86b41ceef15c965bc6df89f34af85
Matt Phillips <mattcphillips17@gmail.com>
2023-08-12 17:28:31 -0700

    actions middleware completed

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,12 +1,32 @@
 // Write your "actions" router here!
 const express = require('express')
+const Action = require('./actions-model')
+const { validateActionID, validateActionBody } = require('./actions-middlware')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'hello from the actions router'
-  })
+router.get('/', (req, res, next) => {
+  Action.get()
+    .then(actions => {
+      res.json(actions)
+    })
+    .catch(next)
+})
+
+router.get('/:id', validateActionID, (req, res) => {
+  res.json(req.action)
+})
+
+router.post('/', validateActionBody, (req, res, next) => {
+  
+})
+
+router.put('/:id', (req, res, next) => {
+  
+})
+
+router.delete('/:id', (req, res, next) => {
+  
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
